eea69132c7de5b5ddeb4324bb19438ceb7338965
PR77 <6563857+PR77@users.noreply.github.com>
2026-05-31 12:56:16 +0200

    Updates
    Builder number.

--- Software/include/firmware_info.h
+++ Software/include/firmware_info.h
@@ -7,9 +7,9 @@
 #define FW_MFG              "PR77"
 #define FW_SLASH_1          " MACRO PAD OR3 "
 #define FW_SLASH_2          "    BY PR77    "
-#define FW_GIT              "e255a82"
-#define FW_BUILD_TIME       "2026-05-26 21:27:04"
-#define FW_BUILD_NUMBER     410
+#define FW_GIT              "a0183d1"
+#define FW_BUILD_TIME       "2026-05-29 22:10:18"
+#define FW_BUILD_NUMBER     416
 
 // https://pid.codes/1209/2345/
 #define USB_VENDOR_ID       0x1209
